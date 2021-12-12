f020f682a448ae1870c88d2beeec2345fc9d0573
DanielPhilosoph <danielph9@gmail.com>
2021-12-12 16:20:49 +0200

    fixed bugs

--- back/routers/user.js
+++ back/routers/user.js
@@ -16,45 +16,84 @@ router.post("/register", (req, res) => {
     req.body.authenticate !== undefined &&
     typeof req.body.authenticate === "boolean"
   ) {
-    bcrypt.genSalt(process.env.ROUNDS, function (_err, salt) {
-      bcrypt.hash(req.body.password, salt, function (_err, hash) {
-        const newSecret = twoFactor.generateSecret({
-          name: req.body.name,
-          hash: hash,
-        });
-        users.push({
-          id: uniqid(),
-          username: req.body.name,
-          hash: hash,
-          salt: salt,
-          authenticate: req.body.authenticate,
-          secret: newSecret,
-        });
-      });
-      res.send("Success");
+    const user = users.find((user) => {
+      return user.username === req.body.name;
     });
+    if (!user) {
+      let id;
+      bcrypt.genSalt(parseInt(process.env.ROUNDS), function (_err, salt) {
+        bcrypt.hash(req.body.password.toString(), salt, function (_err, hash) {
+          // Generate new secret
+          const newSecret = twoFactor.generateSecret({
+            name: "daniel's app",
+            account: req.body.name,
+          });
+          console.log(newSecret);
+          // Generate Id
+          id = uniqid();
+
+          // Add to users
+          users.push({
+            id: id,
+            username: req.body.name,
+            hash: hash,
+            salt: salt,
+            authenticate: req.body.authenticate,
+            secret: newSecret,
+          });
+        });
+        res.json({ register: true, userId: id });
+      });
+    } else {
+      res.status(400).send("Choose different name");
+    }
   } else {
-    res.status(400).send("Missing name / password field");
+    res.status(400).send("Missing name / password / authenticate field");
   }
 });
 
 router.post("/login", (req, res) => {
   if (req.body.name && req.body.password) {
     const user = users.find((user) => {
-      return user.name === req.body.name;
+      return user.username === req.body.name;
     });
-    bcrypt.compare(req.body.password, user.hash, function (err, result) {
-      if (result) {
-        if (user.authenticate) {
-        } else {
-          res.json({ login: true, authenticate: false });
+    console.log(user);
+    // Name exists
+    if (user) {
+      bcrypt.compare(
+        req.body.password.toString(),
+        user.hash,
+        function (err, result) {
+          // Valid password
+          if (result) {
+            if (user.authenticate) {
+              res.json({
+                login: false,
+                authenticate: true,
+                qrLink: user.secret.qr,
+              });
+            } else {
+              res.json({ login: true, authenticate: false, id: user.id });
+            }
+          } else {
+            // Wrong password to user
+            res.status(400).send("Wrong name or password");
+          }
         }
-      } else {
-        // access denied
-      }
-    });
+      );
+    } else {
+      res.status(400).send("Wrong name or password");
+    }
   } else {
     res.status(400).send("Missing name / password field");
+  }
+});
+
+router.post("/code", (req, res) => {
+  if (req.body.code && req.body.id) {
+    twofactor.verifyToken("XDQXYCP5AC6FA32FQXDGJSPBIDYNKK5W", "630618");
+  } else {
+    res.status(400).send("Missing code field");
   }
 });
 
